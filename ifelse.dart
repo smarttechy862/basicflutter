91c45404d8cc96c201fca3f74c030aabf50866a6
RushiNChapadia <54469152+RushiNChapadia@users.noreply.github.com>
2021-10-03 19:34:41 +0530

    Create ifelse.dart

---
+++ ifelse.dart
@@ -0,0 +1,7 @@
+// this checks a single condition, 
+// and the "else" block provides a fallback
+if (inPortland) {
+  print('Bring an umbrella!');
+} else {
+  print('Check the weather first!');
+}
